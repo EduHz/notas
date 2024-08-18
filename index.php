2faeb7a34bc036c8fd22d9ca4604c70f602078ec
edu hz <eduzprogramador@gmail.com>
2024-08-18 14:52:49 -0300

    fucniona bien

--- index.php
+++ index.php
@@ -3,12 +3,12 @@
 </h2>
 
 <form action="notas.php" method="post">
-    <label for="Titulo">Titulo</label>
+    <label for="Titulo">Title</label>
     <input type="text" name="titulo" maxlength="20"></input>
-    <label for="Nota">Nota</label>
-    <input type="text" name="notas"></input>
-    <label for="Autor">Autor</label>
+    <label for="Autor">Auth</label>
     <input type="text" name="autor" maxlength="20"></input>
+    <label for="Nota">Note</label>
+    <input type="text" name="notas"></input>
     <input type="submit">
 </form>
 
--- respuesta.php
+++ respuesta.php
@@ -8,32 +8,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $sql = "UPDATE notas SET titulo='$titulo', notas='$notas', autor='$autor' WHERE id='$id'";
     if ($conn->query($sql) == TRUE) {
-        // echo "nota actualizada <br>";
-        // echo "<a href='notas.php'>Ver todas las notas</a>";
         $sql = "SELECT * from notas where id = $id";
         echo "<h1>Nota actualizada</h1>";
-        echo "<div class='contenedor'>";
-        echo "<div class='nota'>";
-        echo "<div class='fila1'>";
-        echo "<h2>Titulo</h2>";
-        echo "<h3>Notas</h3>";
-        echo "<p>Autor</p>";
-        echo "</div>";
-        echo "<div class='datos'>";
-        echo "<h2> $titulo </h2>";
-        echo "<p> $notas </p>";
-        echo "<p> $autor </p>";
-        echo "</div>";
-        echo "</div>";
-        echo "</div>";
-        echo "<p>todas las notas<p/>";
-        require 'notas.php';
-        // header('Location: notas.php');
+        header('Location: notas.php');
     } else {
         echo "error" . $conn->error;
     }
-    $conn->close();
-    exit();
 }
 
 
